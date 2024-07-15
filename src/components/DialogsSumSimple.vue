041532511efa69d863bf1b07603f3cea3dd69df2
Sylla Xenia <natoire@yandex.ru>
2024-07-15 20:56:47 +0300

    made it simplier

--- src/components/DialogsSumSimple.vue
+++ src/components/DialogsSumSimple.vue
@@ -1,51 +1,46 @@
 <script setup>
 import { ref } from 'vue'
 
-const testData = {
-  'Обработанных ботом': {
-    'абсолютное значение': 37202,
-    'относительное значение': 33.1
+const testData = [
+  {
+    "label": 'Обработанных ботом',
+    'absolut': 37202,
+    'relative': 33.1
   },
-  'Обработанных ботом и оператором': {
-    'абсолютное значение': 20238,
-    'относительное значение': 18.01
+  {
+    "label": 'Обработанных ботом и оператором',
+    'absolut': 20238,
+    'relative': 18.01
   },
-  'Потерянных на операторе': {
-    'абсолютное значение': 371,
-    'относительное значение': 0.33
+  {
+    "label": 'Потерянных на операторе',
+    'absolut': 371,
+    'relative': 0.33
   },
-  'Определено тематик': {
-    'абсолютное значение': 46402,
-    'относительное значение': 24.16
+  {
+    "label": 'Определено тематик',
+    'absolut': 46402,
+    'relative': 24.16
   }
-}
+];
 
 const data = ref(testData)
-console.log(data.value['Обработанных ботом'])
+
 </script>
 
 <template>
-  <DialogSumSimple>
-    <div class="container">
-      <h2 class="title">Количество диалогов</h2>
-      <!--структура под таблицу, rows = 4, column = 3, можно попробовать фигачить через v-for,
+  <div class="container">
+    <h2 class="title">Количество диалогов</h2>
+    <!--структура под таблицу, rows = 4, column = 3, можно попробовать фигачить через v-for,
       например-->
-      <article class="dialogSum">
+    <article class="dialogSum">
+      <template v-for="item in data" :key="item.label">
         <div class="sumTitle">sumTitle</div>
         <div class="absolutSum">absoluteSum</div>
         <div class="relativeSum">relativeSum</div>
-        <div class="sumTitle">sumTitle</div>
-        <div class="absolutSum">absoluteSum</div>
-        <div class="relativeSum">relativeSum</div>
-        <div class="sumTitle">sumTitle</div>
-        <div class="absolutSum">absoluteSum</div>
-        <div class="relativeSum">relativeSum</div>
-        <div class="sumTitle">sumTitle</div>
-        <div class="absolutSum">absoluteSum</div>
-        <div class="relativeSum">relativeSum</div>
-      </article>
-    </div>
-  </DialogSumSimple>
+      </template>
+    </article>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -56,12 +51,15 @@ console.log(data.value['Обработанных ботом'])
   -webkit-box-shadow: -4px 4px 8px 0px rgba(0, 0, 0, 0.2);
   -moz-box-shadow: -4px 4px 8px 0px rgba(0, 0, 0, 0.2);
   box-shadow: -4px 4px 8px 0px rgba(0, 0, 0, 0.2);
+
   .title {
     text-align: center;
   }
+
   .dialogSum {
     display: grid;
     grid-template-columns: 3fr 1fr 1fr;
+
     .sumTitle,
     .absolutSum,
     .relativeSum {
